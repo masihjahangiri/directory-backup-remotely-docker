1a2447e6e5b4efcb5aa502a2b8717a7af7d0b37d
Masih Jahangiri <masihjahan@gmail.com>
2022-07-10 22:31:45 +0430

    feat: improve backup shell script

--- config/backup.sh
+++ config/backup.sh
@@ -8,14 +8,9 @@ mkdir -p /temp/last-backup
 
 rsync -ravp --progress --stats -e "ssh -oStrictHostKeyChecking=no -o ExitOnForwardFailure=yes -p $SSH_PORT -i id_rsa" "$SSH_USERNAME@$SSH_HOST:$REMOTE_SOURCE_PATH/" /temp/last-backup --delete
 
-# ssh -oStrictHostKeyChecking=no -o ExitOnForwardFailure=yes -f -L 1111:localhost:${DB_PORT} ${SSH_USERNAME}@${SSH_HOST} -p ${SSH_PORT} sleep 10
-# PGPASSWORD=${DB_PASSWORD} pg_dump -c -h localhost --port 1111 -U ${DB_USER} ${DB_NAME} -F p -f ${OUTPUT_FILE}
+tar -cvpzf "${OUTPUT_FILE}.tar.gz" /temp/last-backup
 
-tar -cvpzf ${OUTPUT_FILE} /temp/last-backup
+echo "${OUTPUT_FILE}.tar.gz was created:"
+ls -l ${OUTPUT_FILE}.tar.gz
 
-# gzip $OUTPUT_FILE
-
-# echo "${OUTPUT_FILE}.gz was created:"
-# ls -l ${OUTPUT_FILE}.gz
-
-# find $BACKUP_DIR -maxdepth 1 -mtime +$DAYS_TO_KEEP -name "*${FILE_SUFFIX}.gz" -exec rm -rf '{}' ';'
+find $BACKUP_DIR -maxdepth 1 -mtime +$DAYS_TO_KEEP -name "*${FILE_SUFFIX}.tar.gz" -exec rm -rf '{}' ';'
